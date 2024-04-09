e90a56947897d0cc8de5aa91f9a22ca2d494c408
ryosukeoya <engineerxx1256@gmail.com>
2024-04-10 06:15:32 +0900

    update push_gitconfig.sh

--- push_gitconfig.sh
+++ push_gitconfig.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-if [ -e ${PWD}/gitconfig ]; then
-    rm ${PWD}/gitconfig
+if [ -e "${PWD}/gitconfig" ]; then
+    rm "${PWD}/gitconfig"
 fi
-cp ~/.gitconfig ${PWD}/gitconfig
+cp ~/.gitconfig "${PWD}/gitconfig"
 
 git add gitconfig
 git commit -m 'update gitconfig'
