223280fe8b5bb8e408626b98c9b23e8c085d715e
ryosukeoya <engineerxx1256@gmail.com>
2024-04-10 06:28:48 +0900

    fix: push_gitconfig.sh

--- push_gitconfig.sh
+++ push_gitconfig.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-if [ -e "${PWD}/gitconfig" ]; then
-    rm "${PWD}/gitconfig"
+if [ -e "${PWD}/.gitconfig" ]; then
+    rm "${PWD}/.gitconfig"
 fi
-cp ~/.gitconfig "${PWD}/gitconfig"
+cp ~/.gitconfig "${PWD}/.gitconfig"
 
-git add gitconfig
-git commit -m 'update gitconfig'
+git add .gitconfig
+git commit -m 'update .gitconfig'
 git push origin HEAD
