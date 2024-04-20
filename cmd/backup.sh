6c73cf86c5b3aac3d525cb89b58abb497b1861e4
ryosukeoya <engineerxx1256@gmail.com>
2024-04-20 13:16:24 +0900

    update backup.sh

--- cmd/backup.sh
+++ cmd/backup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 backup_dir='/mnt/c/Users/大屋諒恭/OneDrive - Baleen Studio/ドキュメント/backup'
-[ -n "$1" ] && dir="$1-" || dir=''
+dir="$1"
 zip -r "${backup_dir}/${dir}/backup-$(date +'%Y-%m-%d').zip" ./*
