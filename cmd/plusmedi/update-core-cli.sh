e53b4633e62c6e743110984607408a9e377288bd
ryosukeoya <engineerxx1256@gmail.com>
2024-10-25 14:44:52 +0900

    upsert

---
+++ cmd/plusmedi/update-core-cli.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+go install github.com/plusmedi/mhv2-backends/cli/cmd/core-cli@latest
---
+++ cmd/sort-gomod-directives.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+find /home/ryo/ghq/github.com/plusmedi/mhv2-backends -name "go.mod" -type f -exec grep -nH --null -e ^go \{\} + | sort -t ' ' -k 2 -V -r
+
+# find /home/ryo/ghq/github.com/plusmedi/mhv2-backends -name "go.mod" -type f -exec grep -nH --null -e ^go \{\} + | sort -t ' ' -k 2 -V -r | awk '{
+#     version=$2
+#     printf "%s %s\033[34m%s\033[0m\n", $1, "go", version
+# }'
