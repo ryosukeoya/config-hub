90995f4232bd030775069c936979650b3447e6ca
ryosukeoya <engineerxx1256@gmail.com>
2024-10-28 11:25:44 +0900

    upsert

---
+++ cmd/plusmedi/pull-all-repo.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+export DIRENV_DISABLE=1
+echo "export DIRENV_DISABLE=1"
+
+cleanup() {
+  unset DIRENV_DISABLE
+  echo "unset DIRENV_DISABLE"
+}
+
+trap cleanup EXIT ERR
+
+repos=(
+  "/home/ryo/ghq/github.com/plusmedi/mhv2-api-specs"
+  "/home/ryo/ghq/github.com/plusmedi/mhv2-backends"
+  "/home/ryo/ghq/github.com/plusmedi/mhv2-backend-docs"
+  "/home/ryo/ghq/github.com/plusmedi/mhv2-infra"
+  "/home/ryo/ghq/github.com/plusmedi/mhv2-app-api"
+  "/home/ryo/ghq/github.com/plusmedi/c6o-poc-bff"
+  "/home/ryo/ghq/github.com/plusmedi/mhv2-app"
+)
+
+for repo in "${repos[@]}"; do
+  cd "${repo}" || exit
+  git switch main
+  git pull origin main
+done
